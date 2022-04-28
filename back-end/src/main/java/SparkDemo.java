4bcfee6946b96bdc2921e10b6a34e916fb32781e
kaiji <jgu3@mail.sfsu.edu>
2022-04-27 23:15:20 -0700

    finish classwork 10

--- back-end/src/main/java/SparkDemo.java
+++ back-end/src/main/java/SparkDemo.java
@@ -1,3 +1,4 @@
+import static com.mongodb.client.model.Filters.*;
 import static spark.Spark.*;
 
 import com.google.gson.Gson;
@@ -28,7 +29,7 @@ public class SparkDemo {
   public static void main(String[] args) {
     port(1234);
 
-    Map<String,String> users = new HashMap<>(); // TODO remove this and use mongo collection isntead
+//    Map<String,String> users = new HashMap<>(); // TODO remove this and use mongo collection isntead
     // open connection
     MongoClient mongoClient = new MongoClient("localhost", 27017);
     // get ref to database
@@ -36,22 +37,39 @@ public class SparkDemo {
     // get ref to collection
     MongoCollection<Document> usersCollection = db.getCollection("usersCollection");
 
+//    Document doc = new Document("name", "MongoDB")
+//            .append("type", "database")
+//            .append("count", 1)
+//            .append("username", "a")
+//            .append("password", "123");
+////    usersCollection.insertOne(doc);
+
 
     Gson gson = new Gson();
     post("/logIn", (req, res) -> {
       String body = req.body();
       LoginDto loginDto = gson.fromJson(body, LoginDto.class);
       // TODO swap the users data store with usersCollection
-      if (users.get(loginDto.username) != null) {
-        if (users.get(loginDto.username).equals(loginDto.password)) {
+      if (usersCollection != null) {
+//      if (users.get(loginDto.username) != null) {
+        if (usersCollection.find(gt("username", loginDto.username)).first().getObjectId("_id")
+                .equals(usersCollection.find(gt("password", loginDto.password)).first().getObjectId("_id"))) {
+//        if (users.get(loginDto.username).equals(loginDto.password)) {
           return gson.toJson(new LoginResponseDto(true, null));
         } else {
           return gson.toJson(new LoginResponseDto(false, "Invalid password"));
         }
       } else {
-        users.put(loginDto.username, loginDto.password);
+            Document doc = new Document("name", "MongoDB")
+            .append("type", "database")
+            .append("count", 1)
+            .append("username", loginDto.username)
+            .append("password", loginDto.password);
+        usersCollection.insertOne(doc);
+//        users.put(loginDto.username, loginDto.password);
         return gson.toJson(new LoginResponseDto(true, null));
       }
-    });
+    }
+    );
   }
 }
